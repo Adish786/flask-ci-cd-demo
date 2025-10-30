1c1af1b4966db5d3646847a251b1fe6b9e572a22
Adish Ansari <adishansari786@gmail.com>
2025-10-30 11:12:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name="flask-app",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "Flask>=2.3.3",
+        "pytest>=7.4.2",
+        "gunicorn>=21.2.0",
+    ],
+)
